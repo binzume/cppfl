e6993a77928fa122d70ba34e8cc0012866f9af48
Kosuke Kawahira <kawahira@binzume.net>
2015-04-14 00:29:41 +0900

    move to include dir.

---
+++ tests/json_test.cpp
@@ -0,0 +1,40 @@
+#undef NDEBUG
+#include <cassert>
+#include <iostream>
+#include "json.h"
+using namespace std;
+
+#define EPSILON 1e-8
+
+void test() {
+    json::jsvalue v = json::JSON_CONST([
+	    1,null,
+	    [
+	      31 , 32, "asdfg\"hjk" ,
+	      {
+	        "hoge" : -123e6,
+	        "fuga" : 3.14159
+	      },123
+	    ],
+	    null,5,0
+    ]);
+
+    cout << (int)v[2][1] << endl;
+    cout << (string)v[2][2] << endl;
+    cout << v[2][3]["hoge"].to_i() << endl;
+    cout << v[2][3]["fuga"].to_f() << endl;
+    cout << v[1].is_null() << endl;
+
+    assert((int)v[2][1] == 32);
+    assert((string)v[2][2] == "asdfg\"hjk");
+    assert(v[2][3]["hoge"].to_i() == -123000000);
+    assert(fabs(v[2][3]["fuga"].to_f() - 3.14159) < EPSILON);
+    assert(v[1].is_null());
+
+}
+
+int main(int argc, char * argv[])
+{
+	test();
+	return 0;
+}
